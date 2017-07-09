4fd071850eeb9e97baac456a66fbe14c8ec353eb
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-07-09 15:51:16 +0200

    allow for async css loading

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ module.exports = function (opts) {
   var headScript = (opts.script && opts.scriptAsync) ? `<script src="${opts.script}" async></script>` : ''
   var bodyScript = (opts.script && !opts.scriptAsync) ? `<script src="${opts.script}"></script>` : ''
   var favicon = opts.favicon ? `<link rel="icon" href="${opts.favicon}">` : ''
-  var css = opts.css ? `<link rel="stylesheet" href="${opts.css}">` : ''
+  var css = opts.css
+    ? opts.cssAsync
+      ? `<link rel="stylesheet" href="${opts.css}" media="none" onload="if(media!=='all')media='all'">`
+      : `<link rel="stylesheet" href="${opts.css}">`
+    : ''
   var lang = opts.lang || 'en'
   var dir = opts.dir || 'ltr'
   var head = opts.head || ''
